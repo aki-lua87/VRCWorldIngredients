6b835d1809fa2df1d89f07b8575cea291bc6f157
aki_lua87 <aki_lua87@outlook.jp>
2021-08-31 02:18:32 +0900

    いるのいらないのと作りかけのを追加

---
+++ Commons/ucc/IUdonChipsCommons.cs
@@ -0,0 +1,16 @@
+﻿using VRC.Udon.Common.Interfaces;
+
+// TODO: UdonSharp does not yet support inheriting from interfaces
+
+public interface IUdonChipsCommons
+{
+        float GetMoney();
+        void SetMoney(float f);
+        void PushUdonChips();
+        void AddMoney(float f);
+        void SubtractMoney(float f);
+        bool IsAndOver(float f);
+        bool isOverMoney(float f);
+        // void ShowAllPlayerUdonChips(); private
+        float GetMoneyByPlayerID(int id);
+}
